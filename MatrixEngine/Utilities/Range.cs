b3ea734950007e9a83fd1866767bdb75b68ded77
Ofri Haviv <haviv.ofri@gmail.com>
2021-09-29 14:11:16 +0300

    Perlin noise's generation is much faster.

--- MatrixEngine/Utilities/Range.cs
+++ MatrixEngine/Utilities/Range.cs
@@ -5,8 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MatrixEngine.Utilities {
+
     [Serializable]
-    public class Range {
+    public struct Range {
+        public static readonly Range ZeroToOne = new Range(0, 1);
         public float min;
         public float max;
 
--- MatrixEngineTests/Program.cs
+++ MatrixEngineTests/Program.cs
@@ -3,35 +3,52 @@ using MatrixEngine.GameObjects;
 using MatrixEngine.GameObjects.Components;
 using MatrixEngine.GameObjects.Components.PhysicsComponents;
 using MatrixEngine.GameObjects.Components.RenderComponents;
+using MatrixEngine.Utilities;
 using SFML.System;
+using System.Threading.Tasks;
 
 namespace MatrixEngineTests {
 
     public class Program {
 
         public static void Main() {
-            var app = new App("Tests", false, null);
+            var t = new Clock();
 
-            app.scene = new Scene();
+            var p = new PerlinNoise2D(10, 100, Range.ZeroToOne);
 
-            for (int x = 0; x < 10; x++) {
-                for (int y = 0; y < 10; y++) {
-                    app.scene.AddGameObject(new GameObject(
-                        new Vector2f(x, y),
-                        new SpriteRendererComponent("object.png", 16, 10),
-                        new RigidBodyComponent(true)
-                    ));
-                }
+            var task = Task.Run(() => p.Generate());
+
+            while (!task.IsCompleted) {
             }
 
-            app.scene.AddGameObject(new GameObject(
-                new SimplePlayerControllerComponent(10),
-                new SpriteRendererComponent("object.png", 16, 100),
-                new RigidBodyComponent(new Vector2f(), new Vector2f(50, 50)),
-                new RayTesterComponent()
-               ));
+            //p.Generate();
+            t.ElapsedTime.AsSeconds().Log();
 
-            app.Run();
+            "Done".Log();
+
+            //    var app = new App("Tests", false, null);
+
+            //    app.scene = new Scene();
+
+            //    for (int x = 0; x < 10; x++) {
+            //        for (int y = 0; y < 10; y++) {
+            //            app.scene.AddGameObject(new GameObject(
+            //                new Vector2f(x, y),
+            //                new SpriteRendererComponent("object.png", 16, 10),
+            //                new RigidBodyComponent(true)
+            //            ));
+            //        }
+            //    }
+
+            //    app.scene.AddGameObject(new GameObject(
+            //        new SimplePlayerControllerComponent(10),
+            //        new SpriteRendererComponent("object.png", 16, 100),
+            //        new RigidBodyComponent(new Vector2f(), new Vector2f(50, 50)),
+            //        new RayTesterComponent()
+            //       ));
+
+            //    app.Run();
+            //}
         }
     }
 
