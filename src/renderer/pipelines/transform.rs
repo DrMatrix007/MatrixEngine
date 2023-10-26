fe5504018b0afd042eec8b38309947bc71a6d144
Ofri Haviv <haviv.ofri@gmail.com>
2023-10-26 23:48:20 +0300

    change agian

--- src/renderer/pipelines/transform.rs
+++ src/renderer/pipelines/transform.rs
@@ -32,13 +32,7 @@ impl Transform {
             ]);
     }
     pub fn with_position_diff(mut self, position: Vector3<f32>) -> Self {
-        self.mat = &self.mat
-            * Matrix4::from([
-                [1., 0.0, 0.0, 0.0],
-                [0.0, 1., 0.0, 0.0],
-                [0.0, 0.0, 1., 0.0],
-                [*position.x(), *position.y(), *position.z(), 1.],
-            ]);
+        self.apply_position_diff(position);
         self
     }
     pub fn apply_rotation(&mut self, rotation: Vector3<f32>) {
@@ -46,7 +40,7 @@ impl Transform {
     }
 
     pub fn with_rotation(mut self, rotation: Vector3<f32>) -> Self {
-        self.mat = &self.mat * rotation.euler_into_rotation_matrix4();
+        self.apply_rotation(rotation);
         self
     }
 
@@ -61,13 +55,7 @@ impl Transform {
     }
 
     pub fn with_scale(mut self, scale: Vector3<f32>) -> Self {
-        self.mat = &self.mat
-            * Matrix4::from([
-                [*scale.x(), 0.0, 0.0, 0.0],
-                [0.0, *scale.y(), 0.0, 0.0],
-                [0.0, 0.0, *scale.z(), 0.0],
-                [0., 0., 0., 1.],
-            ]);
+        self.apply_scale(scale);
         self
     }
 }
